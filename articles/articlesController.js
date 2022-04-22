f9fbd283e22ac4f88a2cd8c7fa2ac1d7e1990a35
Claudio Vieira <36890797+clacerda@users.noreply.github.com>
2022-04-22 14:54:57 -0300

    inclusão de tine MCE
    Usado para criar opções de edição de textarea.

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+ 
 
 router.get("/articles", (req, res) => {
-    res.send("ROTA DE ARTIGO");
+        res.send("ROTA DE ARTIGO");
 });
 
 
 
-router.get("/admin/articles/new", (req, res) =>{
-    res.send("ROTA PARA CRIAR NOVA ARTIGO");
+router.get("/admin/articles/new", (req, res) => {
+    
+    Category.findAll().then(categories =>{
+         res.render("admin/articles/new", {categories: categories});
+    });
 });
+
+
 
 
 module.exports = router;
