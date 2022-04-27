5d7cb8cea9511a738a2417a110ce4a0c8379e47a
Claudio Vieira <36890797+clacerda@users.noreply.github.com>
2022-04-27 19:57:47 -0300

    Create a login and logout, middlaware, session,etc

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -3,27 +3,28 @@ const router = express.Router();
 const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugify = require("slugify");
+const adminAuth = require("../middlewares/adminAuth");
  
 
-router.get("/admin/articles", (req, res) => {
+router.get("/admin/articles", adminAuth, (req, res) => {
         Article.findAll({
             include: [{model: Category}]
-        }).then((articles) =>{
+        }).then((articles) => {
             res.render("admin/articles/index", {articles: articles});
         })
 });
 
 
 
-router.get("/admin/articles/new", (req, res) => {
+router.get("/admin/articles/new", adminAuth, (req, res) => {
     
     Category.findAll().then(categories =>{
-         res.render("admin/articles/new", {categories: categories});
+         res.render("admin/articles/new",   {categories: categories});
     });
 });
 
 
-router.post("/articles/save", (req, res) =>{
+router.post("/articles/save",  adminAuth, (req, res) =>{
     let title = req.body.title;
     let body = req.body.body;
     let category = req.body.category;
@@ -38,7 +39,7 @@ router.post("/articles/save", (req, res) =>{
     })
 })
 
-router.post("/articles/delete", (req, res) => {
+router.post("/articles/delete",  adminAuth, (req, res) => {
     var id = req.body.id;
 
     if (id != undefined || !isNaN(id)) {
@@ -109,6 +110,42 @@ router.post("/articles/update", (req, res) => {
     });
 
 });
+
+
+router.get("/articles/page/:num", (req, res) =>{
+    let page = req.params.num;
+    let offset = 0;
+
+    if (isNaN(page) || page == 1) {
+        let offset  = 0;
+    } else {
+        offset = (parseInt(page) - 1 ) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        order: [['id', 'DESC']],
+        offset: offset
+    }).then(articles =>{
+
+        var next = true;
+        if (offset + 4 >= articles.count) {
+            next = false;
+        }
+
+        let result = {
+            page: parseInt(page),
+            next: next,
+            articles : articles
+        }
+
+        Category.findAll().then( categories =>{
+            res.render("admin/articles/page",  {result: result, categories: categories});
+        });
+
+        
+    })
+})
 
 
 
--- index.js
+++ index.js
@@ -1,48 +1,102 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const connection = require("./database/database.js");
+const session = require("express-session");
+const connection = require("./database/database");
 
-
-const categoriesController = require("./categories/categoriesController");
-const articlesController = require("./articles/articlesController");
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+const usersController = require("./users/userController");
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const User = require("./users/User");
 
+// View engine
 app.set('view engine','ejs');
+
+// Sessions
+
+app.use(session({
+    secret: "qualquercoisa", cookie: { maxAge: 3000000  }
+}))
+
+// Static
 app.use(express.static('public'));
 
-//body-parser
+//Body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
-//Database
+// Database
 
 connection
     .authenticate()
     .then(() => {
-        console.log('Conexão efetuada com sucesso!');
-    }).catch((error) =>{
+        console.log("Conexão feita com sucesso!");
+    }).catch((error) => {
         console.log(error);
     })
 
 
-app.use("/", categoriesController);
-app.use("/", articlesController);
+app.use("/",categoriesController);    
+app.use("/",articlesController);
+app.use("/",usersController);
 
 
-app.get("/", (req, res) =>{
-    res.render("index");
-});
+app.get("/", (req, res) => {
+    Article.findAll({
+        order:[
+            ['id','DESC']
+        ],
+        limit: 4
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        });
+    });
+})
+
+app.get("/:slug",(req, res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    });
+})
+
+app.get("/category/:slug",(req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then( category => {
+        if(category != undefined){
+            Category.findAll().then(categories => {
+                res.render("index",{articles: category.articles,categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    })
+})
 
 
 app.listen(8080, () => {
-    console.log("servidor rodando");
-});
-
-
-
-
-
+    console.log("O servidor está rodando!")
+})
